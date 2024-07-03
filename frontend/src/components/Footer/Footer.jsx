bb137a079133af14101a7c9e4953e033520e7f18
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-07-03 15:07:20 +0530

    Added Full stack Ecommerce Project

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <p>
+            DishHunt is dedicated to providing fresh, high-quality tomatoes to customers worldwide. Our commitment to excellence drives us to deliver the best produce, ensuring every tomato meets our rigorous standards of taste, nutrition, and sustainability.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="Facebook" />
+            <img src={assets.twitter_icon} alt="Twitter" />
+            <img src={assets.linkedin_icon} alt="LinkedIn" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+917632061200</li>
+            <li>akshay@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
